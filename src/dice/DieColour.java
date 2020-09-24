2dad7a7aa233e4f1ea09bba53fea52b8d90ab32a
Daniel <Newsom>
2020-09-24 06:39:21 +0100

    Danny - Add Coloured Dice

--- src/dice/DieColour.java
+++ src/dice/DieColour.java
@@ -4,14 +4,14 @@ import javafx.scene.paint.Color;
 
 public enum DieColour {
 	
-	BLACK("Black", Color.BLACK),
-	BLUE("Blue", Color.rgb(63,72,204)),
-	GREEN("Green", Color.rgb(0,255,0)),
-	ORANGE("Orange", Color.rgb(255,130,44)),
-	PINK("Pink", Color.PINK),
-	RED("Red", Color.RED),
 	WHITE("White", Color.WHITE),
-	YELLOW("Yellow", Color.YELLOW);
+	BLACK("Black", Color.BLACK),
+	RED("Red", Color.rgb(236,28,36)),
+	GREEN("Green", Color.rgb(0,255,0)),
+	BLUE("Blue", Color.rgb(63,72,204)),
+	ORANGE("Orange", Color.rgb(255,130,44)),
+	PINK("Pink", Color.rgb(255,90,143)),
+	YELLOW("Yellow", Color.rgb(255,242,0));
 	
 	private String descriptionName;
 	private Color colour;
